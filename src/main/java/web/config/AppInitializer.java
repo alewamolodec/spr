2317c6fe028cedd12a4ae1841438a6028d7ff537
Andrey <cobhc900@gmail.com>
2023-02-20 01:54:59 +0300

    Обновлена конфигурация приложения

--- src/main/java/web/config/AppInitializer.java
+++ src/main/java/web/config/AppInitializer.java
@@ -3,18 +3,29 @@ package web.config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    /**
+     * Конфиги приложения - указываем классы с конфигурациями
+     */
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
         return new Class[]{HibernateConfig.class};
     }
 
+    /**
+     * Конфигурации для сервлета - указываем классы с конфигурациями
+     */
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MvcConfig.class};
+    }
+
+    /**
+     *  Рут контекст приложения т.е. localhost:8080, ранее он был localhost:8080/authors и потом надо было ещё раз
+     *  писать /authors чтобы добраться до контролеера, так как урл для контролеера мапится после рут контекста
+     */
     @Override
     protected String[] getServletMappings() {
-        return new String[]{"/authors"};
+        return new String[]{"/"};
     }
 }
